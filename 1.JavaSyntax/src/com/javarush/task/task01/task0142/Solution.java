e5dc85227a81e72339804f6d18e15744a8b57543
Artic <Notgodiyay@gmail.com>
2019-12-27 03:12:18 +0300

    First again

--- 1.JavaSyntax/src/com/javarush/task/task01/task0142/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0142/Solution.java
@@ -8,5 +8,7 @@ public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
         System.out.println("Амиго очень умный");
+
+        System.out.println("abc");
     }
 }
